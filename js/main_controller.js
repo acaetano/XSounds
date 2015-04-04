e0b770cad41080b0dacf19f9dfbedb27ede62f27
Ailton Caetano <ailtoncaetanos@gmail.com>
2015-04-04 06:41:24 -0300

    Starting to change into an AngularJS app

---
+++ js/main_controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ailton on 04/04/2015.
+ */
+
+angular.module('player').controller("MainController", function(){
+
+    var vm = this;
+    vm.songs = [];
+
+});
---
+++ js/player.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ailton on 04/04/2015.
+ */
+
+var player = angular.module('XSounds', []);
+
